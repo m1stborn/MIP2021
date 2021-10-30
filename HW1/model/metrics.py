3b3d69e49cd0987cb0a6c969a280975e42eb380b
m1stborn <kevin1997324@gmail.com>
2021-10-30 18:54:36 +0800

    feat: train and test

---
+++ HW1/model/metrics.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+def mean_iou_score(pred, labels):
+    """
+    Compute mean IoU score over 6 classes
+    """
+    mean_iou = 0
+    for i in range(6):
+        tp_fp = np.sum(pred == i)
+        tp_fn = np.sum(labels == i)
+        tp = np.sum((pred == i) * (labels == i))
+        iou = tp / (tp_fp + tp_fn - tp)
+        mean_iou += iou / 6
+        print('class #%d : %1.5f' % (i, iou))
+    print('\nmean_iou: %f\n' % mean_iou)
+
+    return mean_iou
+
+
+class IOU(object):
+    def __init__(self):
+        self.unions = np.zeros(2)
+        self.tps = np.zeros(2)
+        self.iou = np.zeros(2)
+        self.mean_iou = 0
+
+    def batch_iou(self, pred, labels):
+        """
+        Compute mean IoU score over 6 classes for a batch and update
+        IOU = overlap / union
+        """
+        for i in range(2):
+            tp_fp = np.sum(pred == i)
+            tp_fn = np.sum(labels == i)
+            tp = np.sum((pred == i) * (labels == i))
+
+            self.tps[i] += tp
+            self.unions[i] += (tp_fp + tp_fn - tp)
+
+    def miou(self):
+        mean_iou = 0
+        if self.unions[1] != 0:
+            iou = self.tps[1] / self.unions[1]
+            return iou
+        return mean_iou
---
+++ HW1/utils.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import torch
+import numpy as np
+import skimage.io
+
+
+def save_checkpoint(state, save_path: str):
+    torch.save(state, save_path)
+
+
+def load_checkpoint(ckpt_path):
+    ckpt = torch.load(ckpt_path)
+    return ckpt
+
+
+def progress_bar(count, total, prefix='', suffix=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    # percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write(prefix + '[%s]-Step [%s/%s]-%s\r' % (bar, count, total, suffix))
+    sys.stdout.flush()
+
+    # if count == total:
+    #     print("\n")
+
+
+def experiment_record(*args):
+    with open("./ckpt/log.txt", 'a') as f:
+        print("""=======================================================
+UUID:       {}
+Time:       {}
+Batch size: {}
+Lr:         {} 
+Result:
+    Epoch:      {}
+    Valid IoU:  {}
+=======================================================""".format(*args), file=f)
+
+
+def save_mask(filepath, pred, img_fn, mode='Train'):
+    masks_rgb = np.empty((len(pred), 256, 256, 3))
+    for i, p in enumerate(pred):
+        masks_rgb[i, p == 0] = [0, 0, 0]
+        masks_rgb[i, p == 1] = [255, 255, 255]
+        # masks_rgb[i, p == 2] = [255, 0, 255]
+        # masks_rgb[i, p == 3] = [0, 255, 0]
+        # masks_rgb[i, p == 4] = [0, 0, 255]
+        # masks_rgb[i, p == 5] = [255, 255, 255]
+        # masks_rgb[i, p == 6] = [0, 0, 0]
+    masks_rgb = masks_rgb.astype(np.uint8)
+    for i, mask_rgb in enumerate(masks_rgb):
+        if mode == 'Test':
+            skimage.io.imsave(os.path.join(filepath, img_fn[i]), mask_rgb, check_contrast=False)
+        else:
+            mask_fn = img_fn[i] + '_mask.png'
+            skimage.io.imsave(os.path.join(filepath, mask_fn), mask_rgb, check_contrast=False)
